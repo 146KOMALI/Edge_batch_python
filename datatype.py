e79bc60c0495f18336cf5ff9e8381db5d06cf993
146KOMALI <rekapallisugunanjani@gmail.com>
2026-09-16 19:33:29 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,31 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #data type
+>>> a=2
+>>> type(a)
+<class 'int'>
+>>> b=6.9
+>>> type(b)
+<class 'float'>
+>>> c='code'
+>>> type(c)
+<class 'str'>
+>>> d="komala"
+>>> type(d)
+<class 'str'>
+>>> e='''hasini'''
+>>> type(e)
+<class 'str'>
+>>> f=5+9j
+>>> type(f)
+<class 'complex'>
+>>> g<class 'complex'>
+SyntaxError: invalid syntax
+
+g<class 'complex'>
+SyntaxError: invalid syntax
+
+
+
+
+
