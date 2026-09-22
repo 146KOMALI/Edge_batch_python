95da5f11824bc7a4aae94b77d4b9c2f76d437052
146KOMALI <rekapallisugunanjani@gmail.com>
2026-09-22 22:01:04 +0530

    Add files via upload

---
+++ indexing & slicing .py
@@ -0,0 +1,65 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> #indexing
+>>> a="i am in class"
+>>> a[8]+a[9]+a[10]+a[11]+a[12]
+'class'
+>>> a[2]+a[3]
+'am'
+>>> a[5]+a[6]
+'in'
+>>> a[1]
+' '
+>>> a[4]
+' '
+>>> a[1]+a[4]+a[7]
+'   '
+>>> a="vijayawada"
+>>> a[3]
+'a'
+>>> a[5]
+'a'
+>>> a[1]
+'i'
+>>> a[0]
+'v'
+>>> a[0]+a[1]+a[2]+a[3]+a[4]+a[5]
+'vijaya'
+>>> b="I am learning python fullstack"
+>>> b[13]+b[14]+b[15]+b[16]+b[17]+b[18]
+' pytho'
+>>> b[21]+b[22]+b[23]+b[24]
+'full'
+>>> b[5]
+'l'
+>>> b[5]+b[6]+b[7]+b[8]+b[9]
+'learn'
+>>> b[2]+b[3]
+'am'
+>>> #negative
+>>> a="time is very precious"
+>>> a[-8]+a[-7]+a[-6]+a[-5]+a[-4]+a[-2]+a[-1]
+'precius'
+>>> 'precius'
+'precius'
+a="vijayawada is a royal city"
+a[-4]+a[-3]+a[-2]+a[-1]
+'city'
+a[-10]+a[-9]+a[-8]+a[-7]+a[-6]
+a[-10]+a[-9]+a[-8]+a[-7]+a[-6]
+
+SyntaxError: multiple statements found while compiling a single statement
+a[-10]+a[-9]+a[-8]+a[-7]+a[-6]
+'royal'
+#slicing
+a=
+SyntaxError: invalid syntax
+a="codegnan"
+a[0:4]
+'code'
+a[:4]
+'code'
+a[4:]
+'gnan'
+a[4:8]
+'gnan'
