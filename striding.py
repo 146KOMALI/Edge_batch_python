ba5e3b25a6eeb681b04ba768969cc694b772f2d9
146KOMALI <rekapallisugunanjani@gmail.com>
2026-09-23 20:20:12 +0530

    Add files via upload

---
+++ striding.py
@@ -0,0 +1,52 @@
+Python 3.14.7 (tags/v3.14.7:823f032, Aug  5 2026, 10:51:32) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#striding
+a=machine learning
+SyntaxError: invalid syntax
+a=Data science
+SyntaxError: invalid syntax
+a=Machine Learning
+SyntaxError: invalid syntax
+a="machine learning"
+a[ : : ]
+'machine learning'
+a[ : :4]
+'miln'
+>>> a[ : :2]
+'mcielann'
+>>> a[5:9]
+'ne l'
+>>> a[6:]
+'e learning'
+>>> a[ :11]
+'machine lea'
+>>> a[ : :8]
+'ml'
+>>> a="cloud computing"
+>>> a[1:8:2]
+'lu o'
+>>> a[2:12:4]
+'ocu'
+>>> a[1:13:3]
+'ldou'
+>>> a[3:9:4]
+'uo'
+>>> # the above are positive type of string
+>>> a="python course"
+>>> a[-1:-8:-2]
+'ero '
+>>> a[-2:-12:-3]
+'sont'
+>>> a[-4:-13:-4]
+'uny'
+>>> [6:2:1]
+SyntaxError: invalid syntax
+>>> a[6:2:1]
+''
+>>> a[-8:-4:-2]
+''
+>>> a[ : :1]
+'python course'
+>>> a[ : :-1]
+'esruoc nohtyp'
+>>> #-  --> it indicates negative type of striding
